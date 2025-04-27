c97259a071e7db620c3ceaaf84e87377a13e0634
Tian-Chris <tianchris9810@gmail.com>
2025-04-27 00:31:03 -0700

    revert

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import cv2
  
- cap = cv2.VideoCapture(0)
- cap.read()
- ret, frame = cap.read()
+cap = cv2.VideoCapture(0)
+cap.read()
+ret, frame = cap.read()
  
- if ret:
-     cv2.imwrite("snapshot.jpg", frame)
-     print("photo captured successfully.")
- else:
-     print("failed to capture image.")
+if ret:
+    cv2.imwrite("snapshot.jpg", frame)
+    print("photo captured successfully.")
+else:
+    print("failed to capture image.")
  
- cap.release()
+cap.release()
