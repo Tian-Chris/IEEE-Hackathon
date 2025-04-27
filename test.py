2fe7b0cb9847c3f4619c6b0ef7ec6de5384e4e11
Tian-Chris <tianchris9810@gmail.com>
2025-04-27 00:30:08 -0700

    revert

--- test.py
+++ test.py
@@ -1,21 +1,13 @@
 import cv2
-import os
-
-gst_pipeline = (
-    "libcamerasrc ! "
-    "video/x-raw,width=640,height=480,format=RGB888 ! "
-    "videoconvert ! "
-    "appsink"
-)
-
-cap = cv2.VideoCapture(gst_pipeline, cv2.CAP_GSTREAMER)
-
-ret, frame = cap.read()
-
-if ret:
-    cv2.imwrite('facesFolder/snapshot.jpg', frame)
-    print("succeedded")
-else:
-    print("failed")
-
-cap.release()
+ 
+ cap = cv2.VideoCapture(0)
+ cap.read()
+ ret, frame = cap.read()
+ 
+ if ret:
+     cv2.imwrite("snapshot.jpg", frame)
+     print("photo captured successfully.")
+ else:
+     print("failed to capture image.")
+ 
+ cap.release()
